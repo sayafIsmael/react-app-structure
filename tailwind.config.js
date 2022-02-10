6e7614fdc99f66fa8f267ca245c0920ec41d2cd3
Abu Sayaf Ismail <45027528+sayafIsmael@users.noreply.github.com>
2022-02-10 22:09:40 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+
+    },
+    colors: {
+      primary: '#1A1A1C',
+      white: '#FFFFFF'
+    }
+  },
+  plugins: [],
+}
