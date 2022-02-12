cdfa46c42461a9aa4ccd2a6764caa93dc776f544
sayafismael <bdsayaf@gmail.com>
2022-02-12 15:04:41 +0600

    final version

--- src/componetns/Navbar.js
+++ src/componetns/Navbar.js
@@ -3,23 +3,9 @@ import {
 } from "react-router-dom";
 import Modal from 'react-modal';
 import codes from "../data/country.json"
-import React, { useState } from "react";
-import mobile from 'is-mobile';
+import React, { useEffect, useState } from "react";
+const mobile = require('is-mobile');
 
-const customStyles = {
-    content: {
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)',
-        zIndex: 1000,
-        background: "transeparent",
-        border: 0
-    },
-    overlay: { zIndex: 1000, background: "rgba(91, 97, 110, 0.5)" }
-};
 
 export default () => {
     const [togleMenue, setTogleMenue] = useState(false)
@@ -29,6 +15,27 @@ export default () => {
     function handleCodepicker(e) {
         setCountryCode(e.target.value)
     }
+
+
+    const customStyles = {
+        content: {
+            top: mobile() ? 0 : '50%',
+            left: mobile() ? 0 : '50%',
+            right: mobile() ? 0 : 'auto',
+            bottom: mobile() ? 0 : 'auto',
+            marginRight: mobile() ? 0 : '-50%',
+            transform: mobile() ? '' : 'translate(-50%, -50%)',
+            zIndex: 1000,
+            background: "transeparent",
+            border: 0,
+            borderRadius: 0
+        },
+        overlay: {
+            zIndex: 1000,
+            background: "rgba(91, 97, 110, 0.5)"
+        }
+    };
+
 
     return (
         <header>
@@ -68,24 +75,29 @@ export default () => {
                         <li className="my-6 text-lg"><Link to="#">Trade </Link></li>
                     </ul>
                     <div className="container mx-auto mt-20">
-                        <a className="nav-btn cursor-pointer" onClick={() => setIsOpen(true)}>Sign up today and get no-fee trading for life.</a>
+                        <a className="nav-btn cursor-pointer" onClick={() => { setIsOpen(true);  }}>Sign up today and get no-fee trading for life.</a>
                         <button className="btn-main w-full mt-10">Get the app</button>
                     </div>
                 </div>}
             </nav>
-            <Modal
+            {<Modal
                 isOpen={modalIsOpen}
                 // onAfterOpen={afterOpenModal}
                 onRequestClose={() => setIsOpen(false)}
                 style={customStyles}
                 contentLabel="Example Modal"
             >
-                <div className="absolute -top-0 right-3 cursor-pointer" onClick={() => setIsOpen(false)}>
+                <div className="absolute -top-1 right-3 cursor-pointer sm:hidden md:block" onClick={() => setIsOpen(false)}>
                     <img alt="" src="/images/icon_x-circle.png" />
                 </div>
-                <div className="p-10 mt-10 m-10 rounded-lg bg-white" style={{ maxWidth: 490}}>
-                    <h1 className="mb-5 text-2xl font-bold">Sign up today and get no-fee trading for life.</h1>
-                    <p className="">Get a link sent to your phone to download the app.</p>
+                <div className="p-10 md:mt-10 md:m-10 md:rounded-lg bg-white sm:h-full md:h-auto signup-box">
+                    <div className="absolute -top-0 right-3 cursor-pointer md:hidden md:block" onClick={() => setIsOpen(false)}>
+                        <img alt="" src="/images/icon_x-circle-2.png" />
+                    </div>
+                    <div className="sm:mt-16 md:mt-0">
+                        <h1 className="mb-5 text-2xl font-bold">Sign up today and get no-fee trading for life.</h1>
+                        <p className="">Get a link sent to your phone to download the app.</p>
+                    </div>
                     <div className="flex flex-wrap mt-20 mb-5">
                         <div className="w-full md:w-4/12 mb-6 md:mb-0">
                             <div className="relative">
@@ -106,7 +118,7 @@ export default () => {
                         <button className="btn-main lg:text-lg md:text-md sm:w-full">Send link</button>
                     </div>
                 </div>
-            </Modal>
+            </Modal>}
         </header>
     )
 }
