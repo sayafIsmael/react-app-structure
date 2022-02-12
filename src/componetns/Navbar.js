3ae0ee307dbca063ebf7c7abccf2c2d7bf07ba98
sayafismael <bdsayaf@gmail.com>
2022-02-12 14:05:37 +0600

    signup modal added desktop

--- src/componetns/Navbar.js
+++ src/componetns/Navbar.js
@@ -1,11 +1,33 @@
 import {
     Link
 } from "react-router-dom";
-
+import Modal from 'react-modal';
+import codes from "../data/country.json"
 import React, { useState } from "react";
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        zIndex: 1000,
+        background: "transeparent",
+        border: 0
+    },
+    overlay: { zIndex: 1000, background: "rgba(91, 97, 110, 0.5)" }
+};
 
 export default () => {
     const [togleMenue, setTogleMenue] = useState(false)
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+    const [countryCode, setCountryCode] = useState("US +1")
+
+    function handleCodepicker(e) {
+        setCountryCode(e.target.value)
+    }
 
     return (
         <header>
@@ -32,7 +54,7 @@ export default () => {
 
                     <div className='md:flex justify-end w-3/12 sm:hidden'>
                         <div className="flex ml-auto">
-                            <a className="nav-btn cursor-pointer mr-10 my-auto lg:block md:hidden">Sign up</a>
+                            <a className="nav-btn cursor-pointer mr-10 my-auto lg:block md:hidden" onClick={() => setIsOpen(true)}>Sign up</a>
                             <button className="btn-nav">Get the app</button>
                         </div>
                     </div>
@@ -45,11 +67,45 @@ export default () => {
                         <li className="my-6 text-lg"><Link to="#">Trade </Link></li>
                     </ul>
                     <div className="container mx-auto mt-20">
-                        <a className="nav-btn cursor-pointer">Sign up today and get no-fee trading for life.</a>
-                        <button className="btn-nav w-full mt-10">Get the app</button>
+                        <a className="nav-btn cursor-pointer" onClick={() => setIsOpen(true)}>Sign up today and get no-fee trading for life.</a>
+                        <button className="btn-main w-full mt-10">Get the app</button>
                     </div>
                 </div>}
             </nav>
+            <Modal
+                isOpen={modalIsOpen}
+                // onAfterOpen={afterOpenModal}
+                onRequestClose={() => setIsOpen(false)}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+                <div className="absolute -top-0 right-3 cursor-pointer" onClick={() => setIsOpen(false)}>
+                    <img alt="" src="/images/icon_x-circle.png" />
+                </div>
+                <div className="p-10 mt-10 m-10 rounded-lg bg-white" style={{ maxWidth: 490}}>
+                    <h1 className="mb-5 text-2xl font-bold">Sign up today and get no-fee trading for life.</h1>
+                    <p className="">Get a link sent to your phone to download the app.</p>
+                    <div className="flex flex-wrap mt-20 mb-5">
+                        <div className="w-full md:w-4/12 mb-6 md:mb-0">
+                            <div className="relative">
+                                <select className="text-lg block input-height appearance-none w-full bg-gray-200 text-gray-700 py-3 px-3 rounded leading-tight border border-input-box focus:outline-none focus:border-input-border bg-input-box" id="grid-state"
+                                    value={countryCode} onChange={(e) => handleCodepicker(e)}>
+                                    {codes.map((item, i) => <option key={i}>{item.code} {item.dial_code}</option>)}
+                                </select>
+                                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full md:w-8/12 md:pl-3 mb-6 md:mb-0">
+                            <input className="text-lg appearance-none block input-height w-full bg-gray-200 text-gray-700 rounded px-4 leading-tight border border-input-box focus:outline-none focus:border-input-border bg-input-box" id="grid-city" type="text" placeholder="Enter your phone number" />
+                        </div>
+                    </div>
+                    <div className="w-full mb-20">
+                        <button className="btn-main lg:text-lg md:text-md sm:w-full">Send link</button>
+                    </div>
+                </div>
+            </Modal>
         </header>
     )
 }
